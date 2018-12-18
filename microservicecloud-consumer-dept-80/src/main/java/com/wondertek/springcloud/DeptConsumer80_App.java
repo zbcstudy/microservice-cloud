de15869818e8dbbfc3b4043660d5bad31a7dc1e4
Hasee <1434756304@qq.com>
2018-12-18 22:05:32 +0800

    学习微服务：代码调整

--- microservicecloud-consumer-dept-80/src/main/java/com/wondertek/springcloud/DeptConsumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/wondertek/springcloud/DeptConsumer80_App.java
@@ -1,11 +1,11 @@
 package com.wondertek.springcloud;
 
+import com.wondertek.springcloud.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
-import com.atguigu.myrule.MySelfRule;
 
 @SpringBootApplication
 @EnableEurekaClient
