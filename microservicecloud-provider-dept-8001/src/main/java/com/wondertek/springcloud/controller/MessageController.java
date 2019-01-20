8cb4f6c0657a87ba7d32d14b00fd36f3ce1372eb
Hasee <1434756304@qq.com>
2019-01-20 20:25:08 +0800

    学习微服务：spring cloud config组件学习

--- microservicecloud-provider-dept-8001/src/main/java/com/wondertek/springcloud/controller/MessageController.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/wondertek/springcloud/controller/MessageController.java
@@ -1,6 +1,10 @@
 package com.wondertek.springcloud.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.support.CorrelationData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +19,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/message")
 public class MessageController {
 
+    public static final Logger log = LoggerFactory.getLogger(MessageController.class);
+
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     @GetMapping("/send")
     public String sendMessage(@RequestParam String message) {
+        rabbitTemplate.setConfirmCallback(confirmCallback);
+        rabbitTemplate.setReturnCallback(returnCallback);
         rabbitTemplate.convertAndSend("amq.direct","test",message);
         return "success";
     }
+
+
+    RabbitTemplate.ConfirmCallback confirmCallback = new RabbitTemplate.ConfirmCallback() {
+        @Override
+        public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+            log.info("correlationData: " + correlationData);
+            if (ack) {
+                log.info("-----消息发送成功,correlationData: " + correlationData);
+            }
+        }
+    };
+
+    RabbitTemplate.ReturnCallback returnCallback = new RabbitTemplate.ReturnCallback() {
+        @Override
+        public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
+            log.info("return callBack: message" + message + " replyCode: " + replyCode);
+        }
+    };
 }
--- microservicecloud-provider-dept-8001/src/main/java/com/wondertek/springcloud/rabbitmq/RabbitMqConfig.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/wondertek/springcloud/rabbitmq/RabbitMqConfig.java
@@ -4,11 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.*;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.support.CorrelationData;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -19,33 +14,6 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMqConfig {
 
     public static final Logger log = LoggerFactory.getLogger(RabbitMqConfig.class);
-
-    @Autowired
-    private ConnectionFactory connectionFactory;
-
-    @Bean
-    public RabbitTemplate rabbitTemplate() {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate();
-        rabbitTemplate.setConnectionFactory(connectionFactory);
-        rabbitTemplate.setConfirmCallback(confirmCallback);
-        return rabbitTemplate;
-    }
-
-    RabbitTemplate.ConfirmCallback confirmCallback = new RabbitTemplate.ConfirmCallback() {
-        @Override
-        public void confirm(CorrelationData correlationData, boolean ack, String cause) {
-            if (ack) {
-                log.info("-----消息发送成功,correlationData: " + correlationData);
-            }
-        }
-    };
-
-    RabbitTemplate.ReturnCallback returnCallback = new RabbitTemplate.ReturnCallback() {
-        @Override
-        public void returnedMessage(Message message, int replyCode, String replyText, String exchange, String routingKey) {
-            log.info("return callBack: message" + message + " replyCode: " + replyCode);
-        }
-    };
 
 
     @RabbitHandler
