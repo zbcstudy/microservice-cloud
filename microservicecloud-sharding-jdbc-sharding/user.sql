18148cd58603ebf538c21dd08d6df31fb629f9e8
Hasee <1434756304@qq.com>
2019-03-05 23:29:26 +0800

    sharding jdbc 分库功能添加 sql文件修改

--- microservicecloud-sharding-jdbc-sharding/user.sql
+++ microservicecloud-sharding-jdbc-sharding/user.sql
@@ -1,12 +1,29 @@
-CREATE DATABASE `ds_0` CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
-CREATE DATABASE `ds_1` CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+CREATE DATABASE `ds_2` CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
 
-CREATE TABLE `user`(
+CREATE TABLE `user_0`(
 	id bigint(64) not null,
 	city varchar(20) not null,
 	name varchar(20) not null,
 	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-insert into user values(1001,'上海','尹吉欢');
-insert into user values(1002,'北京','张三');
+CREATE TABLE `user_1`(
+	id bigint(64) not null,
+	city varchar(20) not null,
+	name varchar(20) not null,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_2`(
+	id bigint(64) not null,
+	city varchar(20) not null,
+	name varchar(20) not null,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_3`(
+	id bigint(64) not null,
+	city varchar(20) not null,
+	name varchar(20) not null,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
