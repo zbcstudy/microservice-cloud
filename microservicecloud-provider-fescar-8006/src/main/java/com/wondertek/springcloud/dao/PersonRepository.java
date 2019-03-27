3128b13f333cfe87e89e4053358d4f12c0df6cba
Hasee <1434756304@qq.com>
2019-03-27 23:19:19 +0800

    fescar提交

--- microservicecloud-provider-fescar-8006/src/main/java/com/wondertek/springcloud/dao/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wondertek.springcloud.dao;
-
-import com.wondertek.springcloud.entities.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person,Long> {
-
-}
