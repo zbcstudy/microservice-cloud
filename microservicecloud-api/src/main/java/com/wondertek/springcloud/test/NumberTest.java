f391550f39035e3990632c9fa89363dc6476c3f4
zbcstudy <1434756304@qq.com>
2019-04-02 15:25:19 +0800

    test

---
+++ microservicecloud-api/src/main/java/com/wondertek/springcloud/test/NumberTest.java
@@ -0,0 +1,10 @@
+package com.wondertek.springcloud.test;
+
+/**
+ * Created by win on 2019/4/2.
+ */
+public class NumberTest {
+    public static void main(String[] args) {
+        System.out.println(0x17318);
+    }
+}
